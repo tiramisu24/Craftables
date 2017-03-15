f18a951b54b5bf221bdda2c2fa0b3893fcd4544d
Diane Zheng <diane.z.zheng@gmail.com>
2017-03-15 09:45:02 -0700

    push heroku

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,6 +1,11 @@
 class Api::ProjectsController < ApplicationController
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      render 'api/show/project'
+    else
 
+    end
   end
 
   def update
@@ -9,5 +14,12 @@ class Api::ProjectsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def project_params
+    params[:user][:author_id] = current_user.id
+    params.require(:project).permit(:title, :body, :author_id)
   end
 end
